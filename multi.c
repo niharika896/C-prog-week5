363b2f32e377a1def5260a51f9ff9336825f8447
niharika896 <niharika9agarwal@gmail.com>
2024-12-25 16:18:06 +0530

    Update multi.c

--- multi.c
+++ multi.c
@@ -39,7 +39,7 @@ int main()
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
             for(k=0;k<n;k++){
-            m[i][j]+=m1[i][k]*m2[k][i];
+            m[i][j]+=m1[i][k]*m2[k][j];
             }
         }
     }
